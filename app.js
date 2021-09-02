38a3629fc6e81767299325d11c46510146a5e49b
AnirudhaLokhande <anirudha.lokhande@gmail.com>
2021-09-02 20:07:56 +0530

    data-types

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-console.log("hey this is my fist piece of js code")
+console.log('Hey this is my first piece of JS code!!! YAY')
+
+console.log(12490239043902)
+
+console.log({ name: "Yutika" })
 
 console.log(true)
-console.log('you')
+console.log(false)
+
+console.log(false, 234243, 'string', { name: "Alfred" })
+
+console.warn("This is a warning")
+
+console.error("this is an error")
+
+// Data types
+
+// Primitive data types
+
+let age = 19;
+
+console.log(age)
+
+let brand
+
+console.log(brand)
+
+// Non primitive data types 
+
+let arr = [1,2,3,4]
+console.log(arr)
