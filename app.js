37d675d3694ff0969fa023de68603b82ec25b09a
AnirudhaLokhande <anirudha.lokhande@gmail.com>
2021-09-03 20:18:48 +0530

    Logical-Operators

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-//   Arithmetic operators
+// Logical operators
 
-const a = 2
-const b = 15
+const a = 10
+const b = 50
 
-console.log(a + b)
+const c = 55
 
-console.log(a - b)
+console.log(c > a && c < b)
 
-console.log(a * b)
+// console.log('a == b', a == b)
+// console.log('a != b', a != b)
+// console.log('')
 
-console.log(a / b)
+// console.log('Truth table for AND')
+// console.log('')
 
-console.log(a ** b)
+// console.log('false && false', false && false)
+// console.log('true && true', true && true)
+// console.log('true && false', true && false)
+// console.log('false && true', false && true)
+// console.log('')
 
-console.log(b % a)
+// console.log('Truth table for OR')
+// console.log('')
+
+// console.log('false|| false', false || false)
+// console.log('true|| true', true || true)
+// console.log('true|| false', true || false)
+// console.log('false|| true', false || true)
