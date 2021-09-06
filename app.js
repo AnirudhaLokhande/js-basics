972b0ee9afcf1e3d03caeb80473a430d89390b76
AnirudhaLokhande <anirudha.lokhande@gmail.com>
2021-09-06 18:23:38 +0530

    function

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const button = document.querySelector('button')
-const body = document.querySelector('body')
+// const rock = document.querySelector('#rock')
+// const paper = document.querySelector('#paper')
+// const scissor = document.querySelector('#scissor')
 
-button.addEventListener('click', ()=>{
-    body.classList.toggle('dark')
-})
+// var Name
+
+// console.log(Name)
+
+function logName(Name = "John Doe", age = 18) {
+    // console.log("Nishta")
+    console.log(Name + " You are " + age)
+}
+
+// Never Misspell the function keyword
+// a function can have multiple parameters
+// You can set default values for the parameters
+// Any variable you define inside a function's body is local to that function
+
+function add(a = 0, b = 0) {
+    return a + b
+}
+
+const result = add()
+
+console.log(result)
