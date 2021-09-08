e3d9216691a3e1f695daf1e5974f5fdb0fcce815
AnirudhaLokhande <anirudha.lokhande@gmail.com>
2021-09-08 20:04:50 +0530

    object destructuring

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log("MERGED CLASS 1 ES6")
 var arr = [1,2,3,4];
 console.log(arr[2]);
 
-var [one, two, three, four, five] = [1,2,3,4]; //ES6 Destructuring
+var [one, two, three, four, five] = arr; //ES6 Destructuring
 console.log(three);
 console.log(four);
 console.log(five);
@@ -17,3 +17,29 @@ let [x,y,z] = getscores();
 console.log(x);
 console.log(y);
 console.log(z);
+
+
+const obj = {
+    fullname: "Nikhil Agarwal",
+    email: "nikhilagarwaliitkgp@gmail.com",
+    age: 20
+}
+
+console.log(obj.email)
+
+let {dum, age, email, fullname} = obj;
+console.log(fullname)
+console.log(dum)
+
+
+let person = {
+    firstname: "Nikhil",
+    lastname: "Agarwal",
+    age1: 20,
+    middlename: "DNA"
+}
+let {age1, firstname, lastname, middlename="" } = person;
+console.log(age1);
+console.log(firstname);
+console.log(lastname);
+console.log(middlename);
