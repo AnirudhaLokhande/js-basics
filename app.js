e06a5258af885aa92cd91221b46640e2a9fc0570
AnirudhaLokhande <anirudha.lokhande@gmail.com>
2021-09-09 19:47:53 +0530

    map and filter

--- app.js
+++ app.js
@@ -1,57 +1,30 @@
-console.log("MERGED CLASS 1 ES6");
+console.log("Map, Filter");
 
-var arr = [1, 2, 3, 4];
-console.log(arr[2]);
+let ranks = [1, 2, 3, 4, 5 ,6];
 
-var [one, two, three, four, five] = arr; //ES6 Destructuring
-console.log(three);
-console.log(four);
-console.log(five);
+console.log(
+  ranks.map((e, index) => {
+    return e * 100;
+  })
+);
 
-function getscores() {
-  return [90, 100];
-}
+console.log(
+  ranks.forEach((e, index) => {
+    return e * 100;
+  })
+);
 
-let [x, y, z] = getscores();
-console.log(x);
-console.log(y);
-console.log(z);
+let newmaparr = ranks.map((e, index) => {
+    console.log(e)
+    if(e%2===0)
+        return e
+})
+console.log(newmaparr);
 
-const obj = {
-  fullname: "Nikhil Agarwal",
-  email: "nikhilagarwaliitkgp@gmail.com",
-  age: 20,
-};
 
-console.log(obj.email);
-
-let { dum, age, email, fullname } = obj;
-console.log(fullname);
-console.log(dum);
-
-let person = {
-  firstname: "Nikhil",
-  lastname: "Agarwal",
-  age1: 20,
-  middlename: "DNA",
-};
-let { age1, firstname, lastname, middlename = "" } = person;
-console.log(age1);
-console.log(firstname);
-console.log(lastname);
-console.log(middlename);
-
-// function add(x, y) {
-//     return x+y;
-// }
-// console.log(add(10, 20));
-
-// let add = function(x,y) {
-//     return x+y;
-// }
-// console.log(add(10,20))
-
-let add = (x, y) => {
-  return x + y;
-};
-console.log(add(10, 20));
+let newfilterarr = ranks.filter((e, index) => {
+    console.log(e)
+    if(e%2===0)
+        return e
+})
+console.log(newfilterarr);
